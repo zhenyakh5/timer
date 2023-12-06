1cef8831eeac4be0c78037d5f68d0efb2d6ea2a9
Eugene <zhenya130905@gmail.com>
2023-12-07 02:31:20 +0300

    Added the ability to pause the timer

--- timer/timer/main.cpp
+++ timer/timer/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <iomanip>
+#include <conio.h>  // Добавленная библиотека для _kbhit и _getch
 using namespace std;
 
 int main() {
@@ -11,19 +12,39 @@ int main() {
     cin >> durationInSeconds;
 
     int remainingTime = durationInSeconds;
+    bool isPaused = false;
 
     while (remainingTime > 0) {
         system("cls");
 
-        int hours = remainingTime / 3600;
-        int minutes = (remainingTime % 3600) / 60;
-        int seconds = remainingTime % 60;
+        if (_kbhit()) {  // Проверка на нажатие клавиши
+            char key = _getch();
+            if (key == ' ') {
+                isPaused = !isPaused;
+            }
+        }
 
-        cout << setw(2) << setfill('0') << hours << ":" << setw(2) << setfill('0') << minutes << ":" << setw(2) << setfill('0') << seconds << endl;
+        if (!isPaused) {
+            int hours = remainingTime / 3600;
+            int minutes = (remainingTime % 3600) / 60;
+            int seconds = remainingTime % 60;
 
-        Sleep(1000);
+            cout << setw(2) << setfill('0') << hours << ":" << setw(2) << setfill('0') << minutes << ":" << setw(2) << setfill('0') << seconds << endl;
 
-        remainingTime--;
+            Sleep(1000);
+
+            remainingTime--;
+        } 
+        else {
+            // Если на паузе, все равно покажем текущее время
+            int hours = remainingTime / 3600;
+            int minutes = (remainingTime % 3600) / 60;
+            int seconds = remainingTime % 60;
+
+            cout << "Таймер на паузе. Оставшееся время: " << setw(2) << setfill('0') << hours << ":" << setw(2) << setfill('0') << minutes << ":" << setw(2) << setfill('0') << seconds << endl;
+
+            Sleep(1000);
+        }
     }
 
     system("cls");
